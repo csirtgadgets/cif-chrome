8206f62f97848ad9190b1b14f92a1d8a05b5846a
Kevin Benton <kevin@buttewifi.com>
2012-07-03 16:09:07 -0400

    missing buttons

---
+++ CIF Chrome Extension/content/button.js
@@ -0,0 +1,37 @@
+/**
+ * Installs the toolbar button with the given ID into the given
+ * toolbar, if it is not already present in the document.
+ *
+ * @param {string} toolbarId The ID of the toolbar to install to.
+ * @param {string} id The ID of the button to install.
+ * @param {string} afterId The ID of the element to insert after. @optional
+ */
+function installButton(toolbarId, id, afterId) {
+    if (!document.getElementById(id)) {
+        var toolbar = document.getElementById(toolbarId);
+
+        // If no afterId is given, then append the item to the toolbar
+        var before = null;
+        if (afterId) {
+            let elem = document.getElementById(afterId);
+            if (elem && elem.parentNode == toolbar)
+                before = elem.nextElementSibling;
+        }
+
+        toolbar.insertItem(id, before);
+        toolbar.setAttribute("currentset", toolbar.currentSet);
+        document.persist(toolbar.id, "currentset");
+
+        if (toolbarId == "addon-bar")
+            toolbar.collapsed = false;
+    }
+}
+window.addEventListener("load", function load(event){  
+	if (CIF_CLIENT.getItem('buttoninstalled')===null){
+		installButton("nav-bar", "custom-button-1");
+		// The "addon-bar" is available since Firefox 4
+		installButton("addon-bar", "custom-button-1");
+		CIF_CLIENT.storeItem('buttoninstalled','yes');
+	}
+},false);  
+ 
