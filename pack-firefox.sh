2101f6dcef0737f28e67dc99297f0bb436ae7768
Wes Young <github@barely3am.com>
2013-03-07 14:13:13 -0500

    bugfixes, version bump, fixed firefox stuff

--- pack-firefox.sh
+++ pack-firefox.sh
@@ -4,5 +4,6 @@
 ##
 rm -R -f /tmp/CIF-FFExtension.xpi
 cd './cif-browser-extension'
-zip -r /tmp/CIF-FFExtension.xpi ./
+#zip -r /tmp/CIF-FFExtension.xpi ./
 #cp /tmp/CIF-FFExtension.xpi ~/Desktop/
+zip -r ../CIF-FFExtension.xpi ./
